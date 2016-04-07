29634c83c1a1ccf46f2ad878510331ce217280dc
devrogar <bablu.rohit@hotmail.com>
2016-04-07 06:55:47 +0530

    Added files via upload

---
+++ vocab_gen.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+def main():
+        with open('cleaned_data.txt') as f:
+                lines = f.readlines()
+        one=[]
+        two=[]
+        
+        target=open("vocabulary.txt", 'w')
+
+        for line in lines:
+                line = line.strip().split(" ")
+                for word in line:
+                        if word.isalpha():
+                                if word not in one:
+                                        one.append(word)
+                                elif word not in two:
+                                        two.append(word)
+                                        target.write(word)
+                                        target.write("\n")
+        target.close()
+main()
